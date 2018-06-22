a2c18c82b24237deeeca21b66279deafd519ab1e
Anilkumar Subhash Controllu <FWIN01238@fulcrumww.com>
2018-06-22 11:57:29 +0530

    demo

--- db/mysql-init.sql
+++ db/mysql-init.sql
@@ -2,8 +2,7 @@
 -- create database example_docker_db;
 
 use mkyong
-DROP TABLE IF EXISTS `mkyong`.`EMPLOYEE`;
-CREATE TABLE `mkyong`.`EMPLOYEE`(
+CREATE TABLE EMPLOYEE(
     id INT NOT NULL auto_increment, 
     name VARCHAR(50) NOT NULL,
     joining_date DATE NOT NULL,
