34aeb34529422866fb357919e61730fdca8aa4ed
Anilkumar Subhash Controllu <FWIN01238@fulcrumww.com>
2018-06-22 12:03:32 +0530

    demo

--- db/mysql-init.sql
+++ db/mysql-init.sql
@@ -2,11 +2,11 @@
 -- create database example_docker_db;
 
 use mkyong
-CREATE TABLE EMPLOYEE(
-    id INT NOT NULL auto_increment, 
-    name VARCHAR(50) NOT NULL,
-    joining_date DATE NOT NULL,
-    salary DOUBLE NOT NULL,
-    ssn VARCHAR(30) NOT NULL UNIQUE,
+CREATE TABLE `EMPLOYEE`(
+    `id` INT NOT NULL auto_increment, 
+    `name` VARCHAR(50) NOT NULL,
+    `joining_date` DATE NOT NULL,
+    `salary` DOUBLE NOT NULL,
+    `ssn` VARCHAR(30) NOT NULL UNIQUE,
     PRIMARY KEY (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
